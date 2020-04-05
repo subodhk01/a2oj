decc1ec7879c79aab7f6f18b1e18f48930935fb6
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-05 22:41:06 +0530

    addded whitenoise and staticfiles_dir

--- codeforces/settings.py
+++ codeforces/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static') ,)
-STATIC_ROOT = 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static') ,)
+#STATIC_ROOT = 'static'
 
 
