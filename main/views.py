816035ab2c5c364605a807e1e582265892e82558
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2020-08-31 14:21:54 +0530

    Added solved problems counter

--- main/views.py
+++ main/views.py
@@ -6,65 +6,67 @@ import json
 import requests
 import logging
 
+
 def home(request):
     try:
         handle = request.GET['handle']
         div = request.GET['div']
-        #print(div)
+        # print(div)
     except:
         return render(request, 'index.html')
     parser = codeforces_api.CodeforcesApi()
     try:
         submissions = parser.user_status(handle=handle)
     except:
-        return render(request, 'index.html' , {'msg': 'Invalid Codeforces handle '})
+        return render(request, 'index.html', {'msg': 'Invalid Codeforces handle '})
     ladder = []
     division = []
     div = int(div)
     div_head = ""
-    if div==1:
+    if div == 1:
         division = div_a
         div_head = "DIV 2.A"
-    elif div==2:
+    elif div == 2:
         division = div_b
         div_head = "DIV 2.B"
-    elif div==3:
+    elif div == 3:
         division = div_c
         div_head = "DIV 2.C"
-    elif div==4:
+    elif div == 4:
         division = div_d
         div_head = "DIV 2.D"
-    elif div==5:
+    elif div == 5:
         division = div_e
         div_head = "DIV 2.E"
-    elif div==6:
+    elif div == 6:
         division = div_1d
         div_head = "DIV 1.D"
-    elif div==7:
+    elif div == 7:
         division = div_1e
         div_head = "DIV 1.E"
     else:
-        return render(request, 'index.html' , {'msg': 'Invalid Division selected '})
+        return render(request, 'index.html', {'msg': 'Invalid Division selected '})
+    solved = 0
     for step in division:
         for sub in submissions['result']:
             try:
-                if int(step[2])==sub['problem']['contestId'] and step[3]==sub['problem']['index'] and sub['verdict']=="OK":
-                    ladder.append([ step[0],sub['problem']['name'], sub['problem']['contestId'], sub['problem']['index'], True ])
+                if int(step[2]) == sub['problem']['contestId'] and step[3] == sub['problem']['index'] and sub['verdict'] == "OK":
+                    ladder.append([step[0], sub['problem']['name'], sub['problem']
+                                   ['contestId'], sub['problem']['index'], True])
+                    solved = solved + 1
                     break
             except:
                 pass
         else:
-            ladder.append([ step[0],step[1], step[2], step[3], False ])
+            ladder.append([step[0], step[1], step[2], step[3], False])
     # for step in ladder:
     #     for x in step:
     #         print(x,)
     #     print("\n")
     try:
-        logging.basicConfig(filename='handle.log',level=logging.INFO)
+        logging.basicConfig(filename='handle.log', level=logging.INFO)
         logging.info(handle)
         #r = requests.get('http://kyukey-lock.herokuapp.com/a2oj/'+ handle)
     except:
         print("Error in logging")
-    return render(request, 'ladder.html', {'ladder':ladder, 'division':div_head, 'handle': handle})
-
-    
+    return render(request, 'ladder.html', {'ladder': ladder, 'division': div_head, 'handle': handle, 'solved': solved})
