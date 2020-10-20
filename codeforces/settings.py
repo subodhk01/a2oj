b1a88432e068b3564162e48688b9ccb2e8b72a2e
Aryaman Gupta <aryaman.iitbhu@gmail.com>
2020-10-20 22:09:00 +0530

    fixed some errors

--- codeforces/settings.py
+++ codeforces/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'codeforces.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static') ,)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATIC_ROOT = 'static'
-
-
