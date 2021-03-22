3cc5c9352252644fe366ae287fbcb6bd9a435024
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-03-22 21:04:22 +0800

    Added ladder by rating

--- main/views.py
+++ main/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .ladder import div_a, div_b, div_c, div_d, div_e, div_1d, div_1e
+from .ladder import div_a, div_b, div_c, div_d, div_e, div_1d, div_1e, rating_1, rating_2, rating_3, rating_4, rating_5, rating_6, rating_7, rating_8, rating_9, rating_10, rating_11
 import codeforces_api
 import json
 import requests
 import logging
-
 def home(request):
     try:
         handle = request.GET['handle']
         div = request.GET['div']
+        rating = request.GET['rating']
         #print(div)
     except:
         return render(request, 'index.html')
@@ -21,6 +21,7 @@ def home(request):
     ladder = []
     division = []
     div = int(div)
+    rating = int(rating)
     div_head = ""
     if div==1:
         division = div_a
@@ -43,6 +44,39 @@ def home(request):
     elif div==7:
         division = div_1e
         div_head = "DIV 1.E"
+    elif rating==1:
+        division = rating_1
+        div_head = "Codeforces Rating < 1300"
+    elif rating==2:
+        division = rating_2
+        div_head = "1300 <= Codeforces Rating <= 1399"
+    elif rating==3:
+        division = rating_3
+        div_head = "1400 <= Codeforces Rating <= 1499"
+    elif rating==4:
+        division = rating_4
+        div_head = "1500 <= Codeforces Rating <= 1599"
+    elif rating==5:
+        division = rating_5
+        div_head = "1600 <= Codeforces Rating <= 1699"
+    elif rating==6:
+        division = rating_6
+        div_head = "1700 <= Codeforces Rating <= 1799"
+    elif rating==7:
+        division = rating_7
+        div_head = "1800 <= Codeforces Rating <= 1899"
+    elif rating==8:
+        division = rating_8
+        div_head = "1900 <= Codeforces Rating <= 1999"
+    elif rating==9:
+        division = rating_9
+        div_head = "2000 <= Codeforces Rating <= 2099"
+    elif rating==10:
+        division = rating_10
+        div_head = "2100 <= Codeforces Rating <= 2199"
+    elif rating==11:
+        division = rating_11
+        div_head = "Codeforces Rating >= 2200"
     else:
         return render(request, 'index.html' , {'msg': 'Invalid Division selected '})
     solved = 0
